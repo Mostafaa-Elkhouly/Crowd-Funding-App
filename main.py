45ca700b4e2eb2adcd66e8970bf3158bab24b8c2
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-08-17 22:56:02 +0300

    app main.py

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 #! /usr/bin/python3
 
+import os
+from tabulate import tabulate
+import registration as r
+import login as l
 
+table = [
+    ["Press 1","To Register For New Account"],
+    ["Press 2","If Have Already Existing Account"]
+    ]
+
+while True:
+    val = input(tabulate(table) + "\n" + "Your choose: ")
+
+    try:
+        integer_value = int(val)
+        if integer_value == 1:
+            os.system('clear')
+            r.register_user()
+        elif integer_value == 2:
+            os.system('clear')
+            l.login_user()
+        else:
+            raise ValueError
+
+        break  
+    except ValueError:
+        print("\033[31m" + "Invalid input. Please enter an integer." + "\033[0m")
--- registration.py
+++ registration.py
@@ -2,29 +2,34 @@
 
 import myModules as md
 
+def register_user():
 
-fname = md.read_valid_string_input("Enter Your First Name: ")
-lname = md.read_valid_string_input("Enter Your Last Name: ")
-email = md.read_valid_email("Enter Your Email Address: ")
-password = md.read_valid_password("Enter Your Password: ")
-checkTrue = md.confirm_password("Confirm Your New Password: ", password)
+    print("---------------- registration Form -----------------")
 
-while not checkTrue:
+    fname = md.read_valid_string_input("Enter Your First Name: ")
+    lname = md.read_valid_string_input("Enter Your Last Name: ")
+    email = md.read_valid_email("Enter Your Email Address: ")
+    password = md.read_valid_password("Enter Your Password: ")
+    checkTrue = md.confirm_password("Confirm Your New Password: ", password)
 
-    print("\033[31m" + "Password confirmation failed." + "\033[0m")   
-    password = md.read_valid_password("Please enter your password again: ")
-    if md.confirm_password("Confirm Your New Password: ", password) == True:
-        break
+    while not checkTrue:
 
-mobNumber = md.read_valid_egyptian_mobile_number("Enter Your Mobile Number: ")
+        print("\033[31m" + "Password confirmation failed." + "\033[0m")   
+        password = md.read_valid_password("Please enter your password again: ")
+        if md.confirm_password("Confirm Your New Password: ", password) == True:
+            break
 
-userinfo = {
-    "fname":fname,
-    "lname":lname,
-    "email":email,
-    "passord":password,
-    "mobile":mobNumber
-}
+    mobNumber = md.read_valid_egyptian_mobile_number("Enter Your Mobile Number: ")
 
-md.write_data(userinfo)
+    userinfo = {
+        "fname":fname,
+        "lname":lname,
+        "email":email,
+        "passord":password,
+        "mobile":mobNumber
+    }
+
+    md.write_data(userinfo)
+    print("\033[32m" + f"User {fname} {lname} registered successfully." + "\033[0m")
+
 
